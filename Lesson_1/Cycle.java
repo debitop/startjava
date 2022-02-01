96b7a9096472bb519315dd46424660a42f8db34c
Aleksandr Kochkurkin <info@debitop.com>
2022-02-02 00:34:02 +0300

    Ветвление: if-else 05

--- Lesson_1/Cycle.java
+++ Lesson_1/Cycle.java
@@ -6,7 +6,7 @@ public class Cycle {
             System.out.println(i);
         }
         System.out.println("");
-        
+
         //Выведите на консоль с помощью цикла while все числа от [6, -6] (с шагом итерации 2)
         System.out.println("Выведите на консоль с помощью цикла while все числа от [6, -6] (с шагом итерации 2)");
         int num = 6;
@@ -17,6 +17,14 @@ public class Cycle {
         System.out.println("");
 
         //Подсчитайте с помощью цикла do-while сумму нечетных чисел от [10, 20]. Выведите ее на консоль
-        System.out.println("");
+        System.out.println("Подсчитайте с помощью цикла do-while сумму нечетных чисел от [10, 20]. Выведите ее на консоль");
+        int summ = 0;
+        int num1 = 11;
+        do {
+        summ = summ + num1;
+        num1 = num1 +2;
+        }
+            while(num1 <= 20);
+        System.out.println(summ);    
     }
 }
